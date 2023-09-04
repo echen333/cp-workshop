7ddb2a1a52a8d77bce4cad98d181569d75bd6d1b
mzhang2021 <maxzhang614@gmail.com>
2023-09-03 21:50:18 -0400

    add comment about fast i/o

--- appendix/template.cpp
+++ appendix/template.cpp
@@ -4,6 +4,7 @@
 using namespace std;
 
 int main() {
+    // magic lines to make i/o faster, refer to https://stackoverflow.com/a/31165481 if you're curious why
     ios_base::sync_with_stdio(false);
     cin.tie(NULL);
 
